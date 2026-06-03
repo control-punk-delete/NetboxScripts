4448c58778a62454de82b176a6e74066f29d275b
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-06-03 08:10:51 +0000

    Test

--- GoogleSheetExporter/google_sheet_data_exporter.py
+++ GoogleSheetExporter/google_sheet_data_exporter.py
@@ -1,4 +1,35 @@
 import ipinfo
 from extras.scripts import Script
 from utilities.exceptions import AbortScript
-from ipam.models import IPAddress, IPRange, Prefix
+
+
+
+class GoogleSyncronization(Script):
+    class Meta(Script.Meta):
+        name = "GoogleSheetSynch"
+        description = "Google Sheet synchronizations"
+        scheduling_enabled = False
+
+    def run(self, data, commit):
+        
+
+        TOKEN = data.get("google_sheet_id", None)
+
+        if not TOKEN:
+            self.log_debug("google_sheet_id is not definded.")
+        
+        self.log_debug(data)
+
+        
+        
+        
+
+
+
+
+        
+
+
+              
+        
+
