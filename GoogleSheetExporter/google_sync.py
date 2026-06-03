3b28cf2adea734f49cb377713abd3a9548de815f
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-06-03 12:51:49 +0000

    Google Api Done

--- GoogleSheetExporter/google_sync.py
+++ GoogleSheetExporter/google_sync.py
@@ -2,7 +2,11 @@ from extras.scripts import Script
 from utilities.exceptions import AbortScript
 from ipam.models import IPAddress
 
-
+# Google Libs
+import json
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from googleapiclient.errors import HttpError
 
 class GoogleSyncronization(Script):
     class Meta(Script.Meta):
@@ -10,17 +14,45 @@ class GoogleSyncronization(Script):
         description = "Google Sheet synchronizations"
         scheduling_enabled = False
 
+
+    def append_rows(self, spreadsheet_id, token, rows):
+        token_info = json.loads(token)
+
+        creds = Credentials.from_authorized_user_info(
+            token_info,
+            scopes=["https://www.googleapis.com/auth/spreadsheets"]
+        )
+
+        body = { "values": rows }
+
+
+        try:
+            result = service.spreadsheets().values().append(
+                spreadsheetId=spreadsheet_id,
+                range="Sheet1!A1",
+                valueInputOption="USER_ENTERED",
+                insertDataOption="INSERT_ROWS",
+                body=body
+            ).execute()
+
+            print(f"{result.get('updates').get('updatedRows')} rows added")
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+
+
     def run(self, data, commit):
         
 
-        TOKEN = data.get("google_sheet_id", None)
+        TOKEN = data.get("google_api_token", None)
 
         if not TOKEN:
-            self.log_debug("google_sheet_id is not definded. Aborted!")
+            raise AbortScript("Missing Google API Token.")
+
+        SPREADSHEET_ID = data.get("spreadsheet_id", None)
+
+        if not SPREADSHEET_ID:
             raise AbortScript("Missing Google Sheet ID.")
             
-
-        self.log_debug(f"google_sheet_id defined {TOKEN}")
         
 
         CUSTOM_FIELDS = data.get("custom_fields", None)
@@ -31,17 +63,17 @@ class GoogleSyncronization(Script):
             self.log_debug("Missing IP Addresses that should report")
             raise AbortScript("Missing custom field 'service_ndns_ips' that represent public IP Addresses that connected to ndns.")
 
-        rows = []
+        ROWS = []
 
         for IP in IP_ADDRESSES:
 
             ip_address = IPAddress.objects.get(pk=IP.get("id"))
 
             if ip_address.tags.filter(name="reported").exists():
-                self.log_info(f"IP address {IP.get("display", None)} marked as reported. That means its already exist in table.")
+                self.log_warning(f"IP address {IP.get("display", None)} marked as reported. That means its already exist in table.")
                 continue
             
-            row = [data.get("name", "undefined"), CUSTOM_FIELDS.get("edrpou", "undefined"), IP.get("display", None), IP.get("device", "unknown"), list(ip_address.tags.values_list('name', flat=True))]
+            row = [data.get("name", "undefined"), CUSTOM_FIELDS.get("edrpou", "undefined"), IP.get("display", None), IP.get("dscription", "unknown")]
             self.log_debug(f"New row is created: {row}")
 
             # Помічаємо що в ми опрацювали ці ІР адреси
@@ -51,13 +83,15 @@ class GoogleSyncronization(Script):
                 self.log_debug("Save ip object")
                 ip_address.save()
 
-            rows.append(row)
+            ROWS.append(row)
              
         
         
 
-        self.log_debug(f"Appended {len(rows)} rows: {rows}")
-        
+        self.log_debug(f"Appended {len(ROWS)} rows: {ROWS}")
+        self.log_debug("Start writing to Google Sheet")
+
+        append_rows(self, spreadsheet_id=SPREADSHEET_ID, token=TOKEN ,rows=ROWS)
 
 
         
