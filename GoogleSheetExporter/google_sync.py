27788297b570702c57f20d9f7991349f57f84db3
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-06-03 13:18:18 +0000

    Hot Fix

--- GoogleSheetExporter/google_sync.py
+++ GoogleSheetExporter/google_sync.py
@@ -95,7 +95,7 @@ class GoogleSyncronization(Script):
         self.log_debug("Start writing to Google Sheet")
 
 
-        self.append_rows(spreadsheet_id=SPREADSHEET_ID, token=TOKEN, rows=ROWS)
+        self.append_rows(SPREADSHEET_ID, token=TOKEN, rows=ROWS)
         self.log_success("Done")
 
 
