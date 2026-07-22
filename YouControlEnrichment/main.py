b63d11d9885eb7637bb334ccaf9ecf2253ca78e3
control-punk-delete <cyber.resilience.ua@gmail.com>
2026-07-22 12:46:16 +0000

    YouControl test

--- YouControlEnrichment/main.py
+++ YouControlEnrichment/main.py
@@ -20,36 +20,81 @@ def get_info(edrpou: str = None):
 
 
 def parse(data):
+
+
+    you_control_data = {}
+
     soup = BeautifulSoup(data, "html.parser")
-    seo_table = soup.find("div", {"class": "seo-table"})
+    containers = soup.find_all("div", {"class": "seo-table-contain"})
 
-    results = {} 
-    for row in seo_table.find_all("div", {"class": "seo-table-row"}):
-        key = row.find("div", {"class": "seo-table-col-1"})
-        if key:
-            key = key.get_text().strip().split('\n')[0].strip()
+    for container in containers:
+        seo_table_head = container.find("div", {"class": "seo-table-head"})
+        seo_table_name = seo_table_head.find(["div", "h2"], {"class": "seo-table-name"})
+        seo_table_date = seo_table_head.find("div", {"class": "seo-table-date"})
 
-        v= row.find(["span", "p", "div"], {"class": "seo-table-col-2"})
-        if v:
-            if key in ("Уповноважені особи", "Відомості про осіб, які можуть вчиняти дії від імені юридичної особи, у тому числі підписувати договори, подавати документи для державної реєстрації тощо: прізвище, ім’я, по батькові, дані про наявність обмежень щодо представництва юридичної особи"):
-                continue
-            elif key == "Види діяльності":
-                v = v.get_text().strip().split('\n')[2].strip()
+        if seo_table_name:
+            print(seo_table_name.get_text(strip=True))
 
-            elif key == "Контактна інформація":
-                v = v.get_text().strip().split('\n')[3].strip()
+            if seo_table_date:
+                print(seo_table_date.find("span").get_text())
 
-            elif key == "Дата реєстрації":
-                v = v.get_text().strip().split('\n')[0][0:10]
+        else:
+            continue
 
-            else:
-                v = v.get_text().strip().split('\n')[0].strip()
+        seo_table = container.find("div", {"class": "seo-table"})
+        seo_table_rows = seo_table.find_all("div", {"class": "seo-table-row"})
 
-        if key and v:
-            results.update({key: v})
+        for row in seo_table_rows:
+            key = row.find("div", {"class": "seo-table-col-1"})
+            if key:
+                key = key.get_text().strip().split('\n')[0].strip()
 
-    return(results)
-
+            values = row.find(["span", "p", "div"], {"class": "seo-table-col-2"})
+            if values:
+                values = values.get_text().strip().split('\n')
 
 
-print(parse(get_info("23250933")))
+                if key == "Повне найменування юридичної особи":    
+                    you_control_data["tenant_name_full"] = values[0].strip()
+
+                if key == "Скорочена назва":    
+                    you_control_data["tenant_name"] = values[0].strip()
+
+                if key == "Назва англійською":
+                    you_control_data["tenant_name_en"] = values[0].strip()
+
+                if key == "Статус юридичної особи":
+                    you_control_data["tenant_status"] = values[0].strip()
+
+                if key == "Статус з ЄДР":
+                    you_control_data["tenant_edr_status"] = values[0].strip()
+
+                if key == "Код ЄДРПОУ":
+                    you_control_data["tenant_edrpou"] = values[0].strip()
+
+                if key == "Дата реєстрації":
+                    you_control_data["tenant_registration_date"] = values[0][0:10]
+
+                if key == "Організаційно-правова форма":
+                   you_control_data["tenant_loyal_form"] = values[0].strip()
+
+                if key == "Види діяльності":
+                    you_control_data["tenant_kved_primary"] = values[2].strip()
+                    you_control_data["tenant_kved_secondary"] = list( i for i in values[30:-3] if i!='')
+
+                if key == "Контактна інформація":
+                    you_control_data["tenant_address"] = values[3].strip()
+                    you_control_data["tenant_city"] = you_control_data["tenant_address"].split(",")[-3].strip()
+
+                if key == "Перелік засновників/учасників юридичної особи":
+                    you_control_data["parent_tenant_name"] = values[0].strip()
+                    you_control_data["parent_tenant_edrpou"] = values[2].strip()
+                    you_control_data["parent_tenant_address"] = values[4].strip()
+
+    return you_control_data
+
+
+data = parse(get_info("37225752"))
+
+
+print (data)
---
+++ YouControlEnrichment/nbcs_youcontrol_enrichment.py
@@ -0,0 +1,135 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+from extras.scripts import Script
+from utilities.exceptions import AbortScript
+from tenancy.models import Tenant
+
+
+class YouControlEnrichment(Script):
+    class Meta(Script.Meta):
+        name = "YouControlEnrichment"
+        description = "Збагачення інформації про організацію, шляхом запиту до YouControl"
+        scheduling_enabled = False
+
+    def youcontrol_search_by_edrpou(self, edrpou: str = None):
+        """
+        edrpou - uniq organization identifier
+        return - html page from youcontrol
+        """
+        if not edrpou:
+            raise AbortScript("Missing edrpou argument")
+
+        URL = "https://youcontrol.com.ua/catalog/company_details/"
+        FULL_URL = URL + edrpou + "/"
+
+        headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"}
+        data = requests.get(FULL_URL, headers=headers)
+
+        if data.status_code != 200:
+            raise AbortScript(f"YouControl return status code: {data.status_code}, with {data.text} error")
+    
+        return data.text
+
+
+    def youcontrol_result_parser(self, data):
+        you_control_data = {}
+
+        soup = BeautifulSoup(data, "html.parser")
+        containers = soup.find_all("div", {"class": "seo-table-contain"})
+
+        for container in containers:
+            seo_table_head = container.find("div", {"class": "seo-table-head"})
+            seo_table_name = seo_table_head.find(["div", "h2"], {"class": "seo-table-name"})
+            seo_table_date = seo_table_head.find("div", {"class": "seo-table-date"})
+
+            if seo_table_name:
+                print(seo_table_name.get_text(strip=True))
+
+                if seo_table_date:
+                    print(seo_table_date.find("span").get_text())
+
+            else:
+                continue
+
+            seo_table = container.find("div", {"class": "seo-table"})
+            seo_table_rows = seo_table.find_all("div", {"class": "seo-table-row"})
+
+            for row in seo_table_rows:
+                key = row.find("div", {"class": "seo-table-col-1"})
+                if key:
+                    key = key.get_text().strip().split('\n')[0].strip()
+
+                values = row.find(["span", "p", "div"], {"class": "seo-table-col-2"})
+                if values:
+                    values = values.get_text().strip().split('\n')
+
+
+                    if key == "Повне найменування юридичної особи":    
+                        you_control_data["tenant_name_full"] = values[0].strip()
+
+                    if key == "Скорочена назва":    
+                        you_control_data["tenant_name"] = values[0].strip()
+
+                    if key == "Назва англійською":
+                        you_control_data["tenant_name_en"] = values[0].strip()
+
+                    if key == "Статус юридичної особи":
+                        you_control_data["tenant_status"] = values[0].strip()
+
+                    if key == "Статус з ЄДР":
+                        you_control_data["tenant_edr_status"] = values[0].strip()
+
+                    if key == "Код ЄДРПОУ":
+                        you_control_data["tenant_edrpou"] = values[0].strip()
+
+                    if key == "Дата реєстрації":
+                        you_control_data["tenant_registration_date"] = values[0][0:10]
+
+                    if key == "Організаційно-правова форма":
+                        you_control_data["tenant_loyal_form"] = values[0].strip()
+
+                    if key == "Види діяльності":
+                        you_control_data["tenant_kved_primary"] = values[2].strip()
+                        you_control_data["tenant_kved_secondary"] = list( i for i in values[30:-3] if i!='')
+
+                    if key == "Контактна інформація":
+                        you_control_data["tenant_address"] = values[3].strip()
+                        you_control_data["tenant_city"] = you_control_data["tenant_address"].split(",")[-3].strip()
+
+                    if key == "Перелік засновників/учасників юридичної особи":
+                        you_control_data["parent_tenant_name"] = values[0].strip()
+                        you_control_data["parent_tenant_edrpou"] = values[2].strip()
+                        you_control_data["parent_tenant_address"] = values[4].strip()
+
+        return you_control_data
+
+
+    def run(self, data, commit):
+
+        self.log_debug(data)
+
+        tenant_data = data.get('data', {})
+        tenant_id = tenant_data.get('id')       
+
+        tenant = Tenant.objects.get(pk=tenant_id)
+
+        tenant_edrpou = tenant.cf.get("edrpou")
+        self.log_debug(tenant_edrpou)
+
+        youcontrol_data = self.youcontrol_search_by_edrpou(edrpou = tenant_edrpou)
+
+        if youcontrol_data:
+            youcontrol_parsed_data = self.youcontrol_result_parser(data = youcontrol_data)
+
+        else:
+            self.log_error("YouControl data is missing")
+        
+        self.log_info(youcontrol_parsed_data)
+
+
+        
+
+
+        
